8d69c77f0a466320dd817a4ac23c1d9b87fcac00
wesrobinson12 <wpr124@gmail.com>
2018-02-04 14:13:11 -0500

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: './src/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
